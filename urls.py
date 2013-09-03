bb2b771a90e45cc0ebb2b7d643e0818790c99957
Andrés Méndez <andrex873@gmail.com>
2013-09-03 10:53:29 -0500

    Se agrega modelo recetas con mas complejidad y se configura para ser accedido desde el admin

--- urls.py
+++ urls.py
@@ -1,16 +1,12 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin # Uncomment the next two lines to enable the admin
+from django.conf import settings
 
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-
-    url(r'^$', 'principal.views.lista_bebidas'),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-	url(r'^admin/', include(admin.site.urls)),
+urlpatterns = patterns('',    
+    url(r'^$', 'principal.views.lista_bebidas'), 
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')), # Uncomment the admin/doc line below to enable admin documentation    
+	url(r'^admin/', include(admin.site.urls)), # Uncomment the next line to enable the admin
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'documento_root': settings.MEDIA_ROOT}), 
 )
